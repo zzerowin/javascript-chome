8ff96df2347101ed56b11c6becd69bf67ecc00a9
zzerowin <alaykr123@gmail.com>
2022-04-10 21:15:43 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "d97c24f513361880d18266dd164b6b46";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(`당신은 ${lat} ${lng} 에서 살고 있습니다`);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector('#weather span:first-child');
+    const city = document.querySelector('#weather span:last-child');
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+  });
 }
 
 function onGeoError() {
