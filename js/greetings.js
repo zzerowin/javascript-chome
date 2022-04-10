edec936e2177d5366d29b4378e695f07475cecd7
zzerowin <alaykr123@gmail.com>
2022-04-10 15:39:57 +0900

    Adding ToDos

--- js/greetings.js
+++ js/greetings.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `안녕하세요 ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
